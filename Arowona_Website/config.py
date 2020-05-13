0422953b0524bc1242f7e1a219045d57b152cf4f
Dipo Arowona <abdulsalam.arowona@gmail.com>
2020-05-13 05:32:29 -0400

    Updates to css, layout and html. Also includes new pictures, procfile, config file, and requirements

---
+++ Arowona_Website/config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+class Config():
+    
+    secret_key = '90064593c780c647d5b355bc18583c592253e9973426250d24d5904c5ffbaa64'
+    mail_sever = "smtp.googlemail.com"
+    mail_port = 587
+    mail_use_tls = True
+    mail_username = "noreplyarowona@gmail.com"
+    mail_password = "A6abzcsB9"
