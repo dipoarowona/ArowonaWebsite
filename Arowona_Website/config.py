8b2145cfd007581c62e54f75cb8b9c6e8b952081
Dipo Arowona <abdulsalam.arowona@gmail.com>
2020-11-05 16:33:52 -0500

    Update config.py

--- Arowona_Website/config.py
+++ Arowona_Website/config.py
@@ -2,9 +2,9 @@
 
 class Config():
     
-    secret_key = '90064593c780c647d5b355bc18583c592253e9973426250d24d5904c5ffbaa64'
+    secret_key = ""
     mail_sever = "smtp.googlemail.com"
     mail_port = 587
     mail_use_tls = True
     mail_username = "noreplyarowona@gmail.com"
-    mail_password = "A6abzcsB9"
+    mail_password = ""
